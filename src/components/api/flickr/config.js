ef3b5a0c6bf21842a80719834e6fab38896302ab
IzaacAyelin <47495047+IzaacAyelin@users.noreply.github.com>
2019-08-31 20:22:04 +0300

    Initial dist subtree commit

--- src/components/api/flickr/config.js
+++
@@ -1,8 +0,0 @@
-const apiKey = '522c1f9009ca3609bcbaf08545f067ad';
-export const flickrConfig = {
-    baseUrl: 'https://api.flickr.com/',
-    apiKey: apiKey,
-    endPoints: {
-        images:`services/rest/?method=flickr.photos.search&api_key=${apiKey}&tag_mode=any&per_page=100&format=json&nojsoncallback=1`
-    }
-}
